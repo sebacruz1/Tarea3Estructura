ec0c59c06811d287ef3a726c1f47f88dd1adfbd5
JoakoFuenzalida21 <127423355+JoakoFuenzalida21@users.noreply.github.com>
2023-05-20 18:31:24 -0400

    arreglo minimo

--- main.c
+++ main.c
@@ -345,7 +345,7 @@ int main()
         printf("2.- Establecer precedencia entre tareas\n");
         printf("3.- Mostrar tareas por hacer\n");
         printf("4.- Marcar tarea como completada\n");
-        printf("5.- Deshacer última acción\n");
+        printf("5.- Deshacer Ultima accion\n");
         printf("6.- Cargar datos de tarea desde un archivo de texto\n"); 
         printf("0.- Salir\n");
 
